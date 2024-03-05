a6e5d8932702022be82a334104019a9d6214dcc7
kohta3 <gogonta4@gmail.com>
2024-03-06 00:11:37 +0900

    20240306_update

--- 02_assets/src/js/common.js
+++ 02_assets/src/js/common.js
@@ -1,4 +1,18 @@
-window.onscroll = function() {
-    var scrollHeight = window.pageYOffset || document.documentElement.scrollTop;
-    console.log("現在の高さ：" + scrollHeight + "px");
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const profileElement = document.querySelector('.profile');
+    const navBarElement = document.querySelector('.nav-bar');
+
+    if (profileElement && navBarElement) {
+        const profileClientRect = profileElement.getBoundingClientRect();
+
+        window.onscroll = function() {
+            var scrollHeight = window.pageYOffset || document.documentElement.scrollTop;
+            
+            if (scrollHeight > profileClientRect.top) {
+                navBarElement.style.backgroundColor = "rgba(0,0,0,0.7)";
+            }else{
+                navBarElement.style.backgroundColor = "";
+            }
+        };
+    }
+});
--- component/footer.php
+++ component/footer.php
@@ -1,1 +1,11 @@
+<footer>
+    <div class="footer-img"><img src="/02_assets/images/site_icon/profile_icon.jpg" alt="プロフィールの犬"></div>
+    <div class="footer-content">
+        <h3>webエンジニア オカムラコウタ</h3>
+        <ul>
+            <li>D2C企業でweb制作に従事しており、フロントエンドからサーバーサイドまで幅広く対応しています。</li>
+            <li>お問い合わせは<a href="/contact">こちら</a>からお願いします。</li>
+        </ul>
+    </div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
